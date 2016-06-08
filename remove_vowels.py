ff94365880e9bc01ba1a2000fc8655eb441c70e7
PoovithaK <poovitha.it13@bitsathy.ac.in>
2016-06-08 12:02:13 +0530

    Create remove_vowels.py

---
+++ remove_vowels.py
@@ -0,0 +1,9 @@
+test = 'Program to reverse and remove vowels'
+rev=test[::-1]
+print("Reversed string=")
+print rev
+for eachLetter in rev: 
+        if eachLetter in ['a','e','i','o','u']:
+                rev = rev.replace(eachLetter, '')
+print("String with vowels removed=")
+print rev
